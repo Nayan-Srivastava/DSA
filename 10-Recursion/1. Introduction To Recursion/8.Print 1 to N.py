88830f0592ec22b793ccafc459f216abe36e77e7
Nayan Srivastava <nayansrivastava27@gmail.com>
2026-01-16 18:10:50 +0530

    practice

--- 10-Recursion/1. Introduction To Recursion/8.Print 1 to N.py
+++ 10-Recursion/1. Introduction To Recursion/8.Print 1 to N.py
@@ -2,8 +2,24 @@ def print1ToN(n):
     if(n<=0): # base Case
         return
 
-    print1ToN(n-1) # Recursion Call - Head 
+    print1ToN(n-1) # Recursion Call - Head
 
     print(n)
 
 print1ToN(5)
+
+
+
+def list1ToN(n):
+    """
+    1 to N list
+    """
+    if(n<=0):
+        return []
+
+    smallList = list1ToN(n-1)
+    smallList.append(n)
+
+    return smallList
+
+print(list1ToN(5))
